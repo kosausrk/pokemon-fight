8d8a1dc53a8fbbbf44cb46f27e3bf08f0890f88c
kosausrk <kosausrk@gmail.com>
2025-04-05 01:45:04 -0400

    code

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+function attack() {
+    document.getElementById("battle-log").innerHTML += "<br>Player used Attack!";
+}
+function block() {
+    document.getElementById("battle-log").innerHTML += "<br>Player used Block!";
+}
+function finisher() {
+    document.getElementById("battle-log").innerHTML += "<br>Player used Finisher!";
+}
